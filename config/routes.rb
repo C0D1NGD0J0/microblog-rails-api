4f11f0782ec89bbd326330b7f08c1757d939780e
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-08-13 11:37:35 -0400

    logout feature and registration complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+	resources :sessions, only: [:create]
+	get :logged_in, to: "sessions#logged_in"
+	post "/signup", to: "registrations#create", as: "signup"
+	delete "/logout", to: "sessions#logout"
   root to: "static#home"
 end
