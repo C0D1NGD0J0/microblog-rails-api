4c549e134bc801812987e84f28ad1cd96ffda99e
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-08-13 15:40:31 -0400

    refactored routes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_microblog_app", domain: "domain-placeholder.heroku-app.com"
+if Rails.env == "production"
+	Rails.application.config.session_store :cookie_store, key: "_microblog_app", domain: "domain-placeholder.heroku-app.com"
+else
+	Rails.application.config.session_store :cookie_store, key: "_microblog_app"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-	resources :sessions, only: [:create]
-	get :logged_in, to: "sessions#logged_in"
-	post "/signup", to: "registrations#create", as: "signup"
-	delete "/logout", to: "sessions#logout"
-  root to: "static#home"
+	
+	scope "/api" do
+		resources :login, only: [:create]
+		get :logged_in, to: "sessions#logged_in"
+		post "/signup", to: "registrations#create", as: "signup"
+		delete "/logout", to: "sessions#logout"
+	end
+
+	root to: "static#home"
 end
