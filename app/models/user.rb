dc8089109c36942f8dd72f82bfb929e9999ab22a
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-08-14 11:29:17 -0400

    added roles to users via enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
+	enum role: [:member, :editor, :admin]
+
 	has_secure_password
 	has_many :articles, dependent: :destroy
 
 	validates_presence_of :email
 	validates_uniqueness_of :email
+
+	after_initialize do
+	  if self.new_record? || self.role.nil?
+	    self.role ||= :member
+	  end
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_004709) do
+ActiveRecord::Schema.define(version: 2019_08_14_150447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_08_14_004709) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
   add_foreign_key "articles", "users"
